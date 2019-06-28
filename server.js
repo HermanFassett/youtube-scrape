4716d6c9b3d7e25dfca0c59cb23848aa7295987c
Herman Fassett <hermanfassett@gmail.com>
2019-06-28 09:56:27 -0700

    Simplify lookup and fix incorrect thumbnail and duration

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var app = express();
+const express = require('express');
+const fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
+const app = express();
 
 //Home page
 app.get('/', function(req, res) {
@@ -12,49 +12,42 @@ app.get('/', function(req, res) {
 //API route
 app.get('/api/search', function(req, res){
     // Specify YouTube search url
-    var url = "https://www.youtube.com/results?q=" + req.query.q;
-    if (req.query.page)
-        url += "&page=" + req.query.page;
+    let url = `https://www.youtube.com/results?q=${req.query.q}${req.query.page ? `&page=${req.query.page}` : ''}`;
+
     // Access YouTube search
     request(url, function(error, response, html) {
         // Check for errors
-        if(!error) {
-            var $ = cheerio.load(html);
-            
-            var title, length;
-            var json = { results: [] };
+        if (!error) {
+            const $ = cheerio.load(html);
+                        
+            let json = { results: [] };
 
             $(".yt-lockup-dismissable").each(function(index, vid) {
                 // Get video details
-                var title = $(vid).children().last().children().first().children().first().text();
-                var url = "https://www.youtube.com" + $(vid).children().last().children().first().children().first().attr("href");
-                var duration = /\d+:\d+/.exec($(vid).children().last().children().first().children().last().text());
-                duration = (duration) ? duration[0] : "Playlist";
-                var snippet = $(vid).children().last().children().eq(3).text();
-                var upload_date = $(vid).children().last().children().eq(2).children().first().children().first().text();
-                var views = $(vid).children().last().children().eq(2).children().first().children().last().text().split(" ")[0];
-                var thumbnail = $(vid).children().first().children().first().children().first().children().first().children().first().attr("src");
-                var video = {
-                    "title": title,
-                    "url": url,
-                    "duration": duration,
-                    "snippet": snippet,
-                    "upload_date": upload_date,
-                    "thumbnail_src": thumbnail,
-                    "views": views
+                let $metainfo = $(vid).find(".yt-lockup-meta-info li");
+                let $thumbnail = $(vid).find(".yt-thumb img");
+                let video = {
+                    "title": $(vid).find(".yt-lockup-title").children().first().text(),
+                    "url": `https://www.youtube.com${$(vid).find(".yt-lockup-title").children().first().attr("href")}`,
+                    "duration": $(vid).find(".video-time").text().trim() || "Playlist",
+                    "snippet": $(vid).find(".yt-lockup-description").text(),
+                    "upload_date": $metainfo.first().text(),
+                    "thumbnail_src": $thumbnail.data("thumb") || $thumbnail.attr("src"),
+                    "views": $metainfo.first().text()
                 }
+
                 // Get user details
-                var username = $(vid).children().last().children().eq(1).children().first().text();
-                var user_url = "https://www.youtube.com" + $(vid).children().last().children().eq(1).children().first().attr("href");
-                var verified = $(vid).children().last().children().eq(1).children().last().text() ? false : true;
-                var uploader = {
-                    "username": username,
-                    "url": user_url,
-                    "verified": verified
+                let $byline = $(vid).find(".yt-lockup-byline");
+                let uploader = {
+                    "username": $byline.text(),
+                    "url": `https://www.youtube.com${$byline.find("a").attr("href")}`,
+                    "verified": !!$byline.find("[title=Verified]").length
                 }
+
                 // Send results
                 json.results.push({ video: video, uploader: uploader });
             });
+
             res.json(json);
         }
     });
