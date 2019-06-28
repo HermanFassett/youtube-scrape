0295bd03ccdf18a6d6f4f2f6d3822b941a0b9f6f
Herman Fassett <hermanfassett@gmail.com>
2019-06-28 11:00:08 -0700

    Fix thumbnails and duration and simplify parsing
    * Package updates
    
    * Simplify lookup and fix incorrect thumbnail and duration
    
    * Move to separate file

---
+++ scraper.js
@@ -0,0 +1,50 @@
+const cheerio = require('cheerio');
+const request = require('request');
+
+async function youtube(query, page) {
+    return new Promise((resolve, reject) => {
+        // Specify YouTube search url
+        let url = `https://www.youtube.com/results?q=${query}${page ? `&page=${page}` : ''}`;
+
+        // Access YouTube search
+        request(url, (error, response, html) => {
+            // Check for errors
+            if (!error && response.statusCode === 200) {
+                const $ = cheerio.load(html);
+                            
+                let json = { results: [] };
+    
+                $(".yt-lockup-dismissable").each((index, vid) => {
+                    // Get video details
+                    let $metainfo = $(vid).find(".yt-lockup-meta-info li");
+                    let $thumbnail = $(vid).find(".yt-thumb img");
+                    let video = {
+                        "title": $(vid).find(".yt-lockup-title").children().first().text(),
+                        "url": `https://www.youtube.com${$(vid).find(".yt-lockup-title").children().first().attr("href")}`,
+                        "duration": $(vid).find(".video-time").text().trim() || "Playlist",
+                        "snippet": $(vid).find(".yt-lockup-description").text(),
+                        "upload_date": $metainfo.first().text(),
+                        "thumbnail_src": $thumbnail.data("thumb") || $thumbnail.attr("src"),
+                        "views": $metainfo.first().text()
+                    }
+    
+                    // Get user details
+                    let $byline = $(vid).find(".yt-lockup-byline");
+                    let uploader = {
+                        "username": $byline.text(),
+                        "url": `https://www.youtube.com${$byline.find("a").attr("href")}`,
+                        "verified": !!$byline.find("[title=Verified]").length
+                    }
+    
+                    // Send results
+                    json.results.push({ video: video, uploader: uploader });
+                });
+    
+                return resolve(json);
+            }
+            resolve({ error: error });
+        });
+    });
+}
+
+module.exports.youtube = youtube;
--- server.js
+++ server.js
@@ -1,62 +1,17 @@
-var express = require('express');
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var app = express();
+const express = require('express');
+const scraper = require('./scraper')
+const app = express();
 
 //Home page
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
-})
-//API route
-app.get('/api/search', function(req, res){
-    // Specify YouTube search url
-    var url = "https://www.youtube.com/results?q=" + req.query.q;
-    if (req.query.page)
-        url += "&page=" + req.query.page;
-    // Access YouTube search
-    request(url, function(error, response, html) {
-        // Check for errors
-        if(!error) {
-            var $ = cheerio.load(html);
-            
-            var title, length;
-            var json = { results: [] };
+});
 
-            $(".yt-lockup-dismissable").each(function(index, vid) {
-                // Get video details
-                var title = $(vid).children().last().children().first().children().first().text();
-                var url = "https://www.youtube.com" + $(vid).children().last().children().first().children().first().attr("href");
-                var duration = /\d+:\d+/.exec($(vid).children().last().children().first().children().last().text());
-                duration = (duration) ? duration[0] : "Playlist";
-                var snippet = $(vid).children().last().children().eq(3).text();
-                var upload_date = $(vid).children().last().children().eq(2).children().first().children().first().text();
-                var views = $(vid).children().last().children().eq(2).children().first().children().last().text().split(" ")[0];
-                var thumbnail = $(vid).children().first().children().first().children().first().children().first().children().first().attr("src");
-                var video = {
-                    "title": title,
-                    "url": url,
-                    "duration": duration,
-                    "snippet": snippet,
-                    "upload_date": upload_date,
-                    "thumbnail_src": thumbnail,
-                    "views": views
-                }
-                // Get user details
-                var username = $(vid).children().last().children().eq(1).children().first().text();
-                var user_url = "https://www.youtube.com" + $(vid).children().last().children().eq(1).children().first().attr("href");
-                var verified = $(vid).children().last().children().eq(1).children().last().text() ? false : true;
-                var uploader = {
-                    "username": username,
-                    "url": user_url,
-                    "verified": verified
-                }
-                // Send results
-                json.results.push({ video: video, uploader: uploader });
-            });
-            res.json(json);
-        }
-    });
+//API route
+app.get('/api/search', (req, res) => {
+    scraper.youtube(req.query.q, req.query.page)
+        .then(x => res.json(x))
+        .catch(e => res.send(e));
 });
 
 app.listen(process.env.PORT || 8080, function () {
