eb6cc42c050bd1c326b18667c872dac96febe6a9
Herman Fassett <hermanfassett@gmail.com>
2020-10-23 00:10:35 -0700

    Initial work on page token value

--- scraper.js
+++ scraper.js
@@ -1,85 +1,87 @@
 const cheerio = require('cheerio');
 const request = require('request');
 
-async function youtube(query, page) {
+async function youtube(query, key, pageToken) {
     return new Promise((resolve, reject) => {
+        let json = { results: [], version: require('./package.json').version };
+
         // Specify YouTube search url
-        let url = `https://www.youtube.com/results?q=${encodeURIComponent(query)}${page ? `&page=${page}` : ''}`;
+        if (key) {
+            json["parser"] = "json_format.page_token";
+            json["key"] = key;
+            
+            // Access YouTube search API
+            request.post(`https://www.youtube.com/youtubei/v1/search?key=${key}`, {
+                json: {
+                    context: {
+                        client: {
+                            clientName: "WEB",
+                            clientVersion: "2.20201022.01.01",
+                        },
+                    },
+                    continuation: pageToken
+                },
+            }, (error, response, body) => {
+                console.log(body);
 
-        // Access YouTube search
-        request(url, (error, response, html) => {
-            // Check for errors
-            if (!error && response.statusCode === 200) {
-                const $ = cheerio.load(html);
-                let json = { results: [], version: require('./package.json').version };
-
-                // First attempt to parse old youtube search result style
-                $(".yt-lockup-dismissable").each((index, vid) => {
-                    json["parser"] = "html_format";
-                    json.results.push(parseOldFormat($, vid));
-                });
-
-                // If that fails, we have to parse new format from json data in html script tag
-                if (!json.results.length) {
-                    json["parser"] = "json_format";
-
-                    // Get script json data from html to parse
-                    let data, sectionLists = [];
-                    try {
-                        let match = html.match(/ytInitialData"[^{]*(.*);\s*window\["ytInitialPlayerResponse"\]/s);
-                        if (match && match.length > 1) {
-                            json["parser"] += ".original";
-                        }
-                        else {
-                            json["parser"] += ".scraper_data";
-                            match = html.match(/ytInitialData[^{]*(.*);\s*\/\/ scraper_data_end/s);
-                        }
-                        data = JSON.parse(match[1]);
-                        json["estimatedResults"] = data.estimatedResults || "0";
-                        sectionLists = data.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents;
-                    }
-                    catch(ex) {
-                        console.error("Failed to parse data:", ex);
-                        console.log(data);
-                    }
-
-                    // Loop through all objects and parse data according to type
-                    sectionLists.filter(x => x.hasOwnProperty("itemSectionRenderer")).forEach(sectionList => {
-                        try {
-                            sectionList.itemSectionRenderer.contents.forEach(content => {
-                                try {
-                                    if (content.hasOwnProperty("channelRenderer")) {
-                                        json.results.push(parseChannelRenderer(content.channelRenderer));
-                                    }
-                                    if (content.hasOwnProperty("videoRenderer")) {
-                                        json.results.push(parseVideoRenderer(content.videoRenderer));
-                                    }
-                                    if (content.hasOwnProperty("radioRenderer")) {
-                                        json.results.push(parseRadioRenderer(content.radioRenderer));
-                                    }
-                                    if (content.hasOwnProperty("playlistRenderer")) {
-                                        json.results.push(parsePlaylistRenderer(content.playlistRenderer));
-                                    }
-                                }
-                                catch(ex) {
-                                    console.error("Failed to parse renderer:", ex);
-                                    console.log(content);
-                                }
-                            });
-                        }
-                        catch (ex) {
-                            console.error("Failed to read contents for section list:", ex);
-                            console.log(sectionList);
-                        }
-                    });
+                if (!error && response.statusCode === 200) {
+                    parseJsonFormat(body.onResponseReceivedCommands[0].appendContinuationItemsAction.continuationItems, json);
+                    return resolve(json);
                 }
-    
-                return resolve(json);
-            }
-            resolve({ error: error });
-        });
+                resolve({ error: error });
+            });
+        }
+        else {
+            let url = `https://www.youtube.com/results?q=${encodeURIComponent(query)}`;
+
+            // Access YouTube search
+            request(url, (error, response, html) => {
+                // Check for errors
+                if (!error && response.statusCode === 200) {
+                    const $ = cheerio.load(html);
+
+                    // First attempt to parse old youtube search result style
+                    $(".yt-lockup-dismissable").each((index, vid) => {
+                        json["parser"] = "html_format";
+                        json.results.push(parseOldFormat($, vid));
+                    });
+
+                    // If that fails, we have to parse new format from json data in html script tag
+                    if (!json.results.length) {
+                        json["parser"] = "json_format";
+                        json["key"] = html.match(/"innertubeApiKey":"([^"]*)/)[1];
+
+                        // Get script json data from html to parse
+                        let data, sectionLists = [];
+                        try {
+                            let match = html.match(/ytInitialData"[^{]*(.*);\s*window\["ytInitialPlayerResponse"\]/s);
+                            if (match && match.length > 1) {
+                                json["parser"] += ".original";
+                            }
+                            else {
+                                json["parser"] += ".scraper_data";
+                                match = html.match(/ytInitialData[^{]*(.*);\s*\/\/ scraper_data_end/s);
+                            }
+                            data = JSON.parse(match[1]);
+                            json["estimatedResults"] = data.estimatedResults || "0";
+                            sectionLists = data.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents;
+                        }
+                        catch(ex) {
+                            console.error("Failed to parse data:", ex);
+                            console.log(data);
+                        }
+
+                        // Loop through all objects and parse data according to type
+                        parseJsonFormat(sectionLists, json);
+                    }
+        
+                    return resolve(json);
+                }
+                resolve({ error: error });
+            });
+        }
     });
-}
+};
 
 /**
  * Parse youtube search results from dom elements
@@ -112,6 +114,47 @@ function parseOldFormat($, vid) {
 
     // Return json
     return { video: video, uploader: uploader };
+}
+
+/**
+ * Parse youtube search results from json sectionList array and add to json result object
+ * @param {Array} contents - The array of sectionLists
+ * @param {Object} json - The object being returned to caller
+ */
+function parseJsonFormat(contents, json) {
+    contents.forEach(sectionList => {
+        try {
+            if (sectionList.hasOwnProperty("itemSectionRenderer")) {
+                sectionList.itemSectionRenderer.contents.forEach(content => {
+                    try {
+                        if (content.hasOwnProperty("channelRenderer")) {
+                            json.results.push(parseChannelRenderer(content.channelRenderer));
+                        }
+                        if (content.hasOwnProperty("videoRenderer")) {
+                            json.results.push(parseVideoRenderer(content.videoRenderer));
+                        }
+                        if (content.hasOwnProperty("radioRenderer")) {
+                            json.results.push(parseRadioRenderer(content.radioRenderer));
+                        }
+                        if (content.hasOwnProperty("playlistRenderer")) {
+                            json.results.push(parsePlaylistRenderer(content.playlistRenderer));
+                        }
+                    }
+                    catch(ex) {
+                        console.error("Failed to parse renderer:", ex);
+                        console.log(content);
+                    }
+                });
+            }
+            else if (sectionList.hasOwnProperty("continuationItemRenderer")) {
+                json["nextPageToken"] = sectionList.continuationItemRenderer.continuationEndpoint.continuationCommand.token;
+            }
+        }
+        catch (ex) {
+            console.error("Failed to read contents for section list:", ex);
+            console.log(sectionList);
+        }
+    });
 }
 
 /**
